d456591410af6988c8b7424f2a06807fdf107e78
姚晨嵩 <yaochensong@meituan.com>
2018-11-09 11:06:07 +0800

    update

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script>
-  export default {
-    name: 'downloader',
-  };
+export default {
+    name: 'downloader'
+};
 </script>
 
 <style>
---
+++ src/renderer/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+<div id="wrapper">
+    3333
+</div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+};
+</script>
+
+<style>
+
+</style>
