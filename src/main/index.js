60f77bbff0be36ec6f44d92e2963df6f1bc472c5
姚晨嵩 <yaochensong@meituan.com>
2018-11-09 18:13:29 +0800

    修改下载逻辑

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,6 @@
-import { app, BrowserWindow } from 'electron' // eslint-disable-line
+import { app, BrowserWindow, ipcMain } from 'electron' // eslint-disable-line
+import url from 'url';
+import querystring from 'querystring';
 
 /**
  * Set `__static` path to static files in production
@@ -18,13 +20,54 @@ function createWindow() {
      * Initial window options
      */
     mainWindow = new BrowserWindow({
-        height: 563,
-        useContentSize: true,
-        width: 1000
+        width: 1020,
+        height: 670,
+        frame: false,
+        show: false
     });
 
     mainWindow.loadURL(winURL);
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
+    mainWindow.webContents.session.on('will-download', (event, item) => {
+        const dUrl = item.getURL();
+        const parseObj = url.parse(dUrl);
+        const queryObj = querystring.parse(parseObj.query);
+        const vName = queryObj['v-name'];
+        global[item + vName] = item;
+        // 设置文件存放位置，如果用户没有设置保存路径，Electron将使用默认方式来确定保存路径（通常会提示保存对话框）
+        global[item + vName].setSavePath(`/Users/vamgoon/Desktop/test/${+new Date() + item.getFilename()}`);
+        ipcMain.on('cancel', (event, args) => {
+            global[item + args].pause();
+        });
+        global[item + vName].on('updated', (event, state) => {
+            if (state === 'interrupted') {
+                console.log('Download is interrupted but can be resumed');
+            } else if (state === 'progressing') {
+                if (global[item + vName].isPaused()) {
+                    console.log('Download is paused');
+                } else {
+                    console.log(`Received bytes: ${global[item + vName].getReceivedBytes()}`);
+                }
+            }
+        });
+        global[item + vName].once('done', (event, state) => {
+            if (state === 'completed') {
+                console.log('Download successfully');
+            } else {
+                console.log(`Download failed: ${state}`);
+            }
+        });
+    });
+
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show();
+        mainWindow.focus();
+    });
+    /* eslint-disable */
+    // console.log(mainWindow.webContents);
+    /* eslint-enable */
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
@@ -43,3 +86,9 @@ app.on('activate', () => {
         createWindow();
     }
 });
+
+ipcMain.on('download', (event, args) => {
+    const dUrl = `https://www.nodejs.org/dist/v10.13.0/node-v10.13.0-darwin-x64.tar.gz?v-name=${args}`;
+    mainWindow.webContents.downloadURL(dUrl);
+});
+
--- src/renderer/components/HomePage.vue
+++ src/renderer/components/HomePage.vue
@@ -1,12 +1,25 @@
 <template>
 <div id="wrapper">
-    3333
+    <button @click="download('0001')">点击0001</button>
+    <button @click="cancel('0001')">cancel0001</button>
+    <br>
+    <button @click="download('0002')">点击0002</button>
+    <button @click="cancel('0002')">cancel0002</button>
 </div>
 </template>
 
 <script>
+import { ipcRenderer } from 'electron';// eslint-disable-line
 export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    methods: {
+        download(index) {
+            ipcRenderer.send('download', index);
+        },
+        cancel(index) {
+            ipcRenderer.send('cancel', index);
+        }
+    }
 };
 </script>
 
